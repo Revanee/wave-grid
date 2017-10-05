b2c92dad83b8f3c8bc279def211a0e67e8f60f9c
Revanee <hkamenov123@gmail.com>
2017-10-06 00:20:15 +0200

    Code cleanup

--- sketch.js
+++ sketch.js
@@ -1,81 +1,109 @@
+const MODE = 1 // 1: Wireframe; 2: Cubes
+
+const FLUIDITY = 0.99 // 0 < x < 1 TIP: 1 means no loss in time
+const SPEED = 0.2 // 0 < x < 1 WARNING! High values make the simulation unstable
+const INTERACTION_STRENGTH = -50 // How muck force is given to a clicked point
+const MIN_AMMOUNT = 50 // Ammount of points on the shorter side of the grid
+
+const ASPECT_RATIO = window.innerWidth / window.innerHeight
+
+const GRID_HEIGHT = (ASPECT_RATIO > 1) ? MIN_AMMOUNT : Math.floor(MIN_AMMOUNT / ASPECT_RATIO)
+const GRID_WIDTH = (ASPECT_RATIO > 1) ? Math.floor(MIN_AMMOUNT * ASPECT_RATIO) : MIN_AMMOUNT
+
+const POINT_MARGIN = Math.min(window.innerWidth, window.innerHeight) / Math.min(GRID_HEIGHT, GRID_WIDTH)
+
 const points = []
 
-const gridHeight = 50
-const gridWidth = Math.floor((window.innerWidth / window.innerHeight) * gridHeight)
-
-let getCellWidth = function () {return width / gridWidth}
-let getCellHeight = function () {return height / gridHeight}
-
-setup = function () {
+function setup () {
     createCanvas(window.innerWidth, window.innerHeight, WEBGL)
-    for (let x = 0; x < gridWidth; x++) {
+    for (let x = 0; x < GRID_WIDTH; x++) {
         points.push([])
-        for (let y = 0; y < gridHeight; y++) {
+        for (let y = 0; y < GRID_HEIGHT; y++) {
             points[x].push(new Point())
         }
     }
 }
 
-draw = function () {
+function draw () {
 
     //update
+    calculate()
+    interact()
+    step()
+
+    //draw
+    background(30)
+
+    rotateX(PI * -0.20)
+    translate(0, -height / 10)
+    translate(-width / 2 + POINT_MARGIN / 2, -height / 2 + POINT_MARGIN / 2)
+
+    switch (MODE) {
+        case 1: drawWireframe() 
+        break
+        case 2: drawCubes() 
+        break
+    }
+
+}
+
+function interact () {    
+    if (mouseIsPressed) {
+        let x = Math.floor(mouseX / POINT_MARGIN)
+        let y = Math.floor(mouseY / POINT_MARGIN)
+        if (x >= 0 && y >= 0 && x < GRID_WIDTH && y < GRID_HEIGHT) {
+            points[x][y].nextVal = INTERACTION_STRENGTH
+            points[x][y].force = 0
+        }
+    }
+}
+
+function calculate () {
     points.forEach(function (col, x) {
         col.forEach(function (point, y) {
             let left = points[x - 1] ? points[x - 1][y].val : null
             let right = points[x + 1] ? points[x + 1][y].val : null
             let up = points[x][y - 1] ? points[x][y - 1].val : null
             let down = points[x][y + 1] ? points[x][y + 1].val : null
-            let side = left + right ? left + right : null
-            let vert = up + down ? up + down : null
-            let adj = side + vert ? side + vert : null
+            let adj = left + right + up + down ? left + right + up + down : null
             point.update(adj)
         })
-    })
+    })   
+}
 
-    if (mouseIsPressed) {
-        let x = Math.floor(mouseX / (width / gridWidth))
-        let y = Math.floor(mouseY / (height / gridHeight))
-        if (x >= 0 && y >= 0 && x < gridWidth && y < gridHeight) {
-            points[x][y].nextVal = 50
-            points[x][y].force = 0
-        }
-    }
-
+function step () {
     points.forEach(function (col) {
         col.forEach(function (point) {
             point.step()
         })
     })
+}
 
-    //draw
-    rotateX(PI * -0.20)
-    translate(0, -height / 10)
-    translate(-width / 2 + (width / gridWidth) / 2, -height / 2 + (height / gridHeight) / 2)
-    background(30)
+function drawCubes () {
+    push()
+    points.forEach(function(col, x) {
+        col.forEach(function (point, y) {
+            translate(0, 0, point.val)
+            box(10)
+            translate(0, 0, -point.val)
+            translate(0, POINT_MARGIN)
+        })
+        translate(POINT_MARGIN, 0)
+        translate(0, -POINT_MARGIN * col.length)
+    })
+    pop()
+}
 
-    // push()
-    // points.forEach(function(col, x) {
-    //     col.forEach(function (point, y) {
-    //         translate(0, 0, point.val)
-    //         box(10)
-    //         translate(0, 0, -point.val)
-    //         translate(0, getCellHeight())
-    //     })
-    //     translate(getCellWidth(), 0)
-    //     translate(0, -getCellHeight() * col.length)
-    // })
-    // pop()
-
-    for (let x = 0; x < gridWidth - 1; x++) {
-        fill('#42f4eb')
+function drawWireframe () {
+    fill('#42f4eb')
+    for (let x = 0; x < GRID_WIDTH - 1; x++) {
         beginShape()
-        for (let y = 0; y < gridHeight - 1; y++) {
+        for (let y = 0; y < GRID_HEIGHT - 1; y++) {
             let scl = 5
-            vertex(x * width / gridWidth, y * height / gridHeight, points[x][y].val * scl)
-            vertex((x + 1) *  width / gridWidth, y * height / gridHeight, points[x + 1][y].val * scl)
-            vertex(x * width / gridWidth, (y + 1) * height / gridHeight, points[x][y + 1].val * scl)
-            vertex(x * width / gridWidth, y * height / gridHeight, points[x][y].val * scl)
-            // vertex((x + 1) *  width / gridWidth, (y + 1) * height / gridHeight, points[x + 1][y + 1].val)
+            vertex(x * POINT_MARGIN, y * POINT_MARGIN, points[x][y].val * scl)
+            vertex((x + 1) *  POINT_MARGIN, y * POINT_MARGIN, points[x + 1][y].val * scl)
+            vertex(x * POINT_MARGIN, (y + 1) * POINT_MARGIN, points[x][y + 1].val * scl)
+            vertex(x * POINT_MARGIN, y * POINT_MARGIN, points[x][y].val * scl)
         }
         endShape()
     }
@@ -91,8 +119,8 @@ class Point {
         adj = adj ? adj : 0
         this.force -= this.val
         this.force += ((adj / 2) - this.val)
-        this.nextVal += this.force * 0.1
-        this.force *= 0.99
+        this.nextVal += this.force * SPEED
+        this.force *= FLUIDITY
     }
     step () {
         this.val = this.nextVal
